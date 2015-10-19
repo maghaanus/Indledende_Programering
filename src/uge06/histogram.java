c6c965745b2c64a7c089bfcca7e776495d82f9ae
magnusmisbat <magnusmisbat@10.16.101.140>
2015-10-19 14:12:25 +0200

    Tilføjet While loop

--- src/uge06/histogram.java
+++ src/uge06/histogram.java
@@ -1,17 +1,16 @@
 package uge06;
 
 public class histogram {
-	
+
 	public static void main(String[] args) {
-		
-	int[] m = {1,2,3,4,6,62};
-	
-	
-	
-	System.out.println(m[1]);
-	
-		
-		
-		
+
+		int[] m = { 1, 2, 3, 4, 6, 62 };
+
+		int n = 0;
+
+		while (n < 6) {
+			System.out.println(m[n]);
+			n++;
+		}
 	}
 }
